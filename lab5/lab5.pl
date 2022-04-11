246ca289c6f44a7ad38a539056ec7cb45530429e
dimyasha7123 <d_galushkin@mail.ru>
2022-04-11 11:07:16 +0500

    дописал 5, добавил текст задания

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -14,14 +14,19 @@ my_append([X, L1], L2, [X, L3]) :- my_append(L1, L2, L3).
 
 % task4 - реверс списка
 
-
+deep_reverse([], []).
+deep_reverse([X|L1], L2):- deep_reverse(L1, L3), my_append(L3, [X], L2).
 
 % task5 - проверка на палиндром
 
-
+% использовать реверс
 
 % task6 - получение элемента по индексу
 
 
 
 % task7 - удалить все вхождения элементов
+
+deep_delete(_, [], []).
+deep_delete(X, [X|L], L1):- deep_delete(X, L, L1).
+deep_delete(X, [Y|L], [Y, L1]):- X =\= Y, deep_delete(X, L, L1).
